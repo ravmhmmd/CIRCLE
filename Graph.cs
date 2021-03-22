189107c3aa3822688e836bb37886b5f4c7b0b87d
bintangpananjung <68179028+bintangpananjung@users.noreply.github.com>
2021-03-22 10:55:26 +0700

    Update Graph.cs
    add bfs

--- Graph.cs
+++ Graph.cs
@@ -76,24 +76,90 @@ namespace TubesStima2
                 addEdge(node1, node2);
             }
         }
-        class Program
+        public void printQueue(Queue<string> queue)
         {
-            static void Main(string[] args)
-            {
-                Graph mutual = new Graph();
-                mutual.addVertex("mia");
-                mutual.addVertex("mia");
-                mutual.addVertex("bambang");
-                mutual.addEdge("mia", "bambang");
-                mutual.addVertex("diap");
-                mutual.addVertex("sadjo");
-                mutual.addEdge("diap", "sadjo");
-                mutual.addEdge("sadjo", "bambang");
-                mutual.printadjacencyList();
-                Graph recom = new Graph();
-                recom.loadFile("friend.txt");
-                recom.printadjacencyList();
+            foreach (string name in queue)
+                Console.Write(name);
+        }
+        public void BFS(Queue<string> queue,Queue<string> visited, int limit)
+        {
+            string node;
+            int level = 0;
+            int tempLevel = 0;
+            int countElmtinLevel = 0;
+            int countForLoop = 0;
+            string adjacentnode;
+            while (queue.Count != 0 && level<limit){
+                node = queue.Dequeue();
+                for (int i=0; i<adjacencyList.Find(v => v.name == node).edges.Count; i++)
+                {
+                    adjacentnode = adjacencyList.Find(v => v.name == node).edges[i];
+                    if (!visited.Contains(adjacentnode))
+                    {   
+                        queue.Enqueue(adjacentnode);
+                        if (level == limit - 2)
+                        {
+                            visited.Enqueue(adjacentnode);
+                        }
+                        if (countForLoop == 0)
+                        {
+                            if (level == 0)
+                            {
+                                countElmtinLevel++;
+                            }
+                        }
+                        else
+                        {
+                            tempLevel++;
+                        }
+                    }   
+                }
+                countForLoop++;
+                if (countForLoop > 0)
+                {
+                    if (countForLoop == countElmtinLevel)
+                    {
+                        level++;
+                        countForLoop = 0;
+                        countElmtinLevel = tempLevel;
+                        tempLevel = 0;
+                    }
+                }
             }
+            printQueue(visited);
+        }
+        public void BFSSearch(string name)
+        {
+            
+        }
+        public void recommendation(string akun)
+        {
+            
+        }
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph mutual = new Graph();
+            mutual.addVertex("mia");
+            mutual.addVertex("mia");
+            mutual.addVertex("bambang");
+            mutual.addEdge("mia", "bambang");
+            mutual.addVertex("diap");
+            mutual.addVertex("sadjo");
+            mutual.addEdge("diap", "sadjo");
+            mutual.addEdge("sadjo", "bambang");
+            mutual.printadjacencyList();
+            Graph recom = new Graph();
+            recom.loadFile("friend.txt");
+            recom.printadjacencyList();
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("A");
+            Queue<string> visited = new Queue<string>();
+            visited.Enqueue("A");
+            recom.BFS(queue, visited,2);
         }
     }
 
